71a087aba58ee6bf579884416adc617afdfe70a3
Bruno Oliniski <brunooliniski@gmail.com>
2024-09-12 17:24:46 -0300

    Create Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import { CreateTask } from './components/CreateTask'
 import { Header } from './components/Header'
 import { Tasks } from './components/Tasks'
 import './global.css'
-import { TaskType } from './components/Tasks.tsx';
 
-const tasks: TaskType[] = [
-  { 
-    id: 1,
-    text: 'Estudar React' },
-  { 
-    id: 2,
-    text: 'Ler um livro' },
-  { 
-    id: 3,
-    text: 'Tomar banho' },
-  { 
-    id: 4,
-    text: 'Fazer exercícios' },
-]
 
 function App() {
+
   return (
     <>
       <Header />
-      <CreateTask />
-      <Tasks tasks={tasks}/>
+      <Tasks />
     </>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,48 +1,107 @@
 import styles from './Tasks.module.css';
 import listIcon from '../assets/ListIcon.svg';
-import { Task } from './Task.tsx';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Plus, Trash } from '@phosphor-icons/react';
 
-interface TasksProps {
-  tasks: TaskType[];
+export interface TasksProps {
+  taskList: TaskType[];
 }
 export interface TaskType {
   id: number;
   text: string;
+  done: boolean;
 }
 
-export function Tasks({ tasks }: TasksProps) {
-  const hasTasks = false; 
-  let taskList;
+export function Tasks() {
+  const [taskList, setTaskList] = useState<TaskType[]>([
+    { 
+      id: 1,
+      text: 'Estudar React',
+      done: true,
+    },
+    { 
+      id: 2,
+      text: 'Ler um livro',
+      done: false,
+    },
+    { 
+      id: 3,
+      text: 'Tomar banho',
+      done: true,
+    },
+  ]);
 
-  if (hasTasks) {
-    taskList = 
-    <div className={styles.emptyList}>
-      <img src={listIcon} alt="Um ícone de uma prancheta com uma listagem" />
-      <div>
-        <p className={styles.bold}>Você ainda não tem tarefas cadastradas</p>
-        <p>Crie tarefas e organize seus itens a fazer</p>
-      </div>
-    </div>;
-  } else {
-    taskList = 
-    <div className={styles.taskList}>
-      {tasks.map(task => {
-        return <Task text={task.text} key={task.id} />
-      })}
-    </div>;
+  const [ newTaskText, setNewTaskText ] = useState('')
+
+  function handleCreateTask(event: FormEvent) {
+    event.preventDefault();
+    setTaskList([...taskList, { 
+      id: Math.random() + 1, 
+      text: newTaskText,
+      done: false,
+    }]);
+    setNewTaskText('');
   }
 
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  const hasTasks = taskList.length > 0; 
+  const tasksCount = taskList.length;
+  const tasksFinished = taskList.filter(task => task.done === true).length;
+
   return (
+    <>
+    <div className={styles.createTask}>
+      <form onSubmit={handleCreateTask}>
+        <input 
+          type="text"
+          placeholder="Adicione uma nova tarefa" 
+          onChange={handleNewTaskChange}
+          value={newTaskText}
+        />
+        <button type="submit">
+          Criar
+          <Plus className={styles.createIcon}/>
+        </button>
+      </form>
+    </div>
+
     <div className={styles.wrapper}>
       <div className={styles.status}>
         <p className={styles.createdTasks}>
-          Tarefas criadas <span>0</span>
+          Tarefas criadas <span>{ tasksCount }</span>
         </p>
         <p className={styles.finishedTasks}>
-          Concluídas <span>0</span>
+          Concluídas <span> { tasksFinished }</span>
         </p>
       </div>
-      {taskList}
+
+    {!hasTasks ? 
+      <div className={styles.emptyList}>
+        <img src={listIcon} alt="Um ícone de uma prancheta com uma listagem" />
+        <div>
+          <p className={styles.bold}>Você ainda não tem tarefas cadastradas</p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </div>
+    : 
+      <div className={styles.taskList}>
+        {taskList.map(task => {
+          return (
+            <div className={styles.task} key={task.id}>
+              <input type="checkbox" defaultChecked={task.done} />
+              <p> {task.text} </p>
+              <p className={styles.delete}>
+              <Trash />
+              </p>
+            </div>
+          )
+        })}
+      </div>
+    }
     </div>
+    </>
   )
 }
